c582fae6b60553e0c55439bf98a4219be5a0b44c
Nekmo <contacto@nekmo.com>
2024-03-31 11:12:33 +0200

    Changes by Siro

--- main.py
+++ main.py
@@ -6,22 +6,4 @@ Created on Sat Mar 30 18:30:01 2024
 """
 
 if __name__ == '__main__':
-    image_names = [
-        [
-            'a-a-a-a.jpg',
-            'a-a-a-a-ballena.jpg',
-            'a-a-a-a-barco.jpg',
-        ],
-        [
-            'a-t-a-t-canal.jpg',
-            'a-t-a-t-istmo.JPG',
-            't-a-a-a.jpg',
-        ],
-        [
-            't-t-a-a.jpg',
-            't-t-t-a.jpg',
-            't-t-t-t.jpg',
-        ],
-    ]
-
-    generate_final_image(image_names, 3, 3)
+   pass
