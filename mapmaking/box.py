aa2f04c8251bf31b89b49dfbbce26dbb108dddde
Nekmo <contacto@nekmo.com>
2024-03-31 11:12:11 +0200

    Changes by Irene.

--- mapmaking/box.py
+++ mapmaking/box.py
@@ -17,41 +17,12 @@ class Box:
     def get_boxes_around(self) -> list["Box"]:
         return self.map.boxes_around(self.x, self.y)
 
-    def get_number_of_boxes_around(self) -> int:
-        return len(self.get_boxes_around())
-
     def get_filled_boxes_around(self) -> list["Box"]:
-        return list(filter(lambda box: all(len(box.possible_tiles) == 1) ,self.get_boxes_around()))
-
-    def get_relative_position_of_given_box(self, box: "Box") -> Literal["down", "up", "left", "right"]:
-        if self.x == box.x:
-            if self.y == box.y + 1:
-                return "down"
-            if self.y == box.y - 1:
-                return "up"
-        if self.y == box.y:
-            if self.x == box.x + 1:
-                return "left"
-            if self.x == box.x - 1:
-                return "right"
+        return list(filter(lambda box: len(box.possible_tiles) == 1,
+                           self.get_boxes_around()))
 
     @property
-    def entropy(self) -> Optional[int]:
-        # primero calcula con 8 box colindantes
-        # luego calcula bordes
-        boxes_around = self.get_filled_boxes_around()
-        self.possible_tiles = # create_tile_list()
-        for box in boxes_around:
-            # check up
-            relative_position = self.get_relative_position_of_given_box(box)
-            other_tile = box.possible_tiles[0]
-            for possible_tiles in self.possible_tiles:
-                list(map(lambda is_possible: ))
-
-
-
-        if len(self.possible_tiles) == 0:
-            return None
+    def entropy(self) -> int:
         return len(self.possible_tiles)
 
     def update_entropy_around(self):
@@ -63,9 +34,22 @@ class Box:
         self.possible_tiles = [tile]
         self.update_possible_tiles_around()
 
-    def update_possible_tiles_around(self):
-        for box in self.get_boxes_around():
+    def set_random_tile(self):
+        self.set_tile(choice(self.possible_tiles))
+
+    def get_smallest_entropy_box(self, boxes: ["Box"]) -> "Box":
+        for box in boxes:
             box.update_possible_tiles()
+        box_entropy_list = [box.entropy for box in boxes]
+        index_of_smallest_entropy = box_entropy_list.index(min(box_entropy_list))
+        return boxes[index_of_smallest_entropy]
+
+    def update_possible_tiles_around(self):
+        if len(self.possible_tiles) == 1:
+            return
+        smallest_entropy_box = self.get_smallest_entropy_box(self.get_boxes_around())
+        smallest_entropy_box.update_possible_tiles()
+        smallest_entropy_box.set_random_tile()
 
     def update_possible_tiles(self):
         boxes_around = self.get_filled_boxes_around()
--- mapmaking/map.py
+++ mapmaking/map.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+from typing import Literal
 
 from mapmaking.box import Box
 from mapmaking.tiles import TILES_LIST
@@ -15,11 +16,30 @@ class Map(list):
         self[:] = [[Box(self, x, y) for y in range(self.width)] for x in range(self.height)]
 
     def boxes_around(self, x, y):
-        return [
-            self[i][j] for i in range(x - 1, x + 2)
-            for j in range(y - 1, y + 2) if 0 <= i < self.height
-                                            and 0 <= j < self.width and (i, j) != (x, y)
-        ]
+        boxes = []
+        if x != 0:
+            boxes.append(self[x- 1][y])
+        if x != self.width - 1:
+            boxes.append(self[x + 1][y])
+        if y != 0:
+            boxes.append(self[x][y-1])
+        if y != self.height - 1:
+            boxes.append(self[x][y+1])
+
+        return boxes
+
+    def get_relative_position_of_given_box(self, box1: "Box", box2: "Box") -> Literal["down", "up", "left", "right"]:
+        if box1.x == box2.x:
+            if box1.y == box2.y + 1:
+                return "down"
+            if box1.y == box2.y - 1:
+                return "up"
+        if box1.y == box2.y:
+            if box1.x == box2.x + 1:
+                return "left"
+            if box1.x == box2.x - 1:
+                return "right"
+        pass
 
     def set_random_tile(self):
         box = self[randint(0, self.height)][randint(0, self.width)]
